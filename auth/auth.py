2de33a8013cd37d97414d0e74bbcdb897bce31c1
Dutta, Deepjyoti (Deepjyoti) **CTR** <duttad@avaya.com>
2020-07-03 20:25:51 -0500

    moving the Auth config to envronment setting

--- auth/auth.py
+++ auth/auth.py
@@ -4,10 +4,14 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 import sys
+import os
 
-AUTH0_DOMAIN = 'dev-br-wgpeo.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'castingagency'
+#AUTH0_DOMAIN = 'dev-br-wgpeo.us.auth0.com'
+#ALGORITHMS = ['RS256']
+#API_AUDIENCE = 'castingagency'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 # AuthError Exception
 class AuthError(Exception):
---
+++ setup.sh
@@ -0,0 +1,3 @@
+export AUTH0_DOMAIN='dev-br-wgpeo.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='castingagency'
